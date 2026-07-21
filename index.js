9107984efd580e73aabad15607f7675a3f5b1a57
Mahendra <mahendraballa2005@gmail.com>
2026-07-21 09:01:38 +0530

    made changes for PORT , url with home  and package.json

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const path = require('path');
 dotEnv.config()
 
 const app = express()
-const PORT = 4000;
+const PORT =process.env.PORT || 4000;
 
 mongoose.connect(process.env.MONGO_URL)
     .then(() => {
@@ -32,6 +32,6 @@ app.listen(PORT, ()=>{
     console.log(`server is running at ${PORT }`);
 });
 
-app.use('/home', (req , res)=>{
+app.use('/      ', (req , res)=>{
     res.send("<h1>Hello welcome to Suby</h1>")
 })
